d254cd97910b62d3c3afad63d6ea5f45411bf7d1
seoa <andrew.d.seo@gmail.com>
2020-07-01 21:41:35 -0700

    added easy start, Map for storing edge squares

--- backend/Board.js
+++ backend/Board.js
@@ -7,6 +7,7 @@ export default class Board {
         this._lost = false;
         this.num_unopened_squares = 480;
         this._won = (this.num_unopened_squares <= 99);
+        this.edge_squares = new Map(); // keys are ids, values are Squares
     }
 
     build_grid() {
@@ -18,13 +19,29 @@ export default class Board {
         }
     }
 
+    solve_edges() {
+        let solvable = true;
+        while(solvable) {
+            
+        }
+    }
+
     reveal_squares(pos) {
         let curr_square = this.grid[pos[0]][pos[1]];
+        if(this.edge_squares.has(curr_square.id)) {
+            this.edge_squares.delete(curr_square.id);
+        }
         if(curr_square.surr_bombs > 0 || curr_square.revealed || curr_square.bomb) {
             curr_square.revealed = true;
             if(curr_square.bomb == true) {
                 this._lost = true;
+                return;
             }
+            this.surr_squares(pos).forEach((square) => {
+                if(!square.revealed && !square.flagged) {
+                    this.edge_squares.set(square.id, square);
+                }
+            })
             return;
         }
         this.surr_squares(pos).forEach((square) => {
@@ -33,12 +50,18 @@ export default class Board {
         });
 
     }
-    // no bomb allowed at pos
+    // no bomb allowed at pos or 8 surrounding squares
     set_bombs(pos) {
         for(let i=0; i<99; i++) {
             let row = Math.random() * 16 | 0;
             let col = Math.random() * 30 | 0;
-            if(this.grid[row][col].bomb || (row == pos[0] && col == pos[1])) {
+            let surr = false;
+            this.surr_squares_pos_only(pos).forEach((surr_pos) => {
+                if(row == surr_pos[0] && col == surr_pos[1]) {
+                    surr = true;
+                }
+            });
+            if(this.grid[row][col].bomb || (row == pos[0] && col == pos[1]) || surr) {
                 i--;
             }
             else {
@@ -71,6 +94,20 @@ export default class Board {
             let new_col = pos[1] + dir[1];
             if(new_row >= 0 && new_row < 16 && new_col >= 0 && new_col < 30) {
                 squares.push(this.grid[new_row][new_col]);
+            }
+        })
+        return squares;
+    }
+
+    //returns array of valid surrounding square positions around a pos
+    surr_squares_pos_only(pos) {
+        const DIRS = [[0,1],[1,1],[1,0],[0,-1],[-1,1],[1,-1],[-1,0],[-1,-1]];
+        let squares = [];
+        DIRS.forEach( (dir) => {
+            let new_row = pos[0] + dir[0];
+            let new_col = pos[1] + dir[1];
+            if(new_row >= 0 && new_row < 16 && new_col >= 0 && new_col < 30) {
+                squares.push([new_row, new_col]);
             }
         })
         return squares;
