83e543f8aeaadb4d8e6b86b9d6de16ce97fefdcb
seoa <andrew.d.seo@gmail.com>
2020-07-08 16:36:46 -0700

    bomb fix?

--- frontend/tile.jsx
+++ frontend/tile.jsx
@@ -79,7 +79,7 @@ export default class Tile extends React.Component{
         if(square.revealed) {
             if(square.bomb) {
                 return(
-                    <img onContextMenu={this.flag} className="bomb" src="./images/MS bomb.png"/>
+                    <img onContextMenu={this.flag} className="bomb" src="images/MS bomb.png"/>
                 )
             }
             else{
